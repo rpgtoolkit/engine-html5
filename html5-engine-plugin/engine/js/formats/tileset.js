f488693c0d04100dd89e11962a98fdd726907272
swordmaster2k <b00056835@gmail.com>
2017-04-06 21:47:57 +0100

    Converted engine to use new TileSet JSON format for rendering.
    Signed-off-by: swordmaster2k <b00056835@gmail.com>

--- html5-engine-plugin/engine/js/formats/tileset.js
+++ html5-engine-plugin/engine/js/formats/tileset.js
@@ -1,36 +1,36 @@
-function tileset(filename) {
-  this.file = new binaryIO(filename);
+function Tileset(filename) {
+    // TODO: Make the changes here that chrome suggests.
+    var req = new XMLHttpRequest();
+    req.open("GET", filename, false);
+    req.overrideMimeType("text/plain; charset=x-user-defined");
+    req.send(null);
 
-  this.version = this.file.readInt16();
-  this.count = this.file.readInt16();
-  this.detail = this.file.readInt16();
-
-  this.tiles = [];
-
-  var tile, tileRow;
-
-  // loop through tiles
-  for (var i = 0; i < this.count; i++) {
-    tile = [];
-
-    // rows
-    for (var x = 0; x < rpgtoolkit.tileSize; x++) {
-      tileRow = [];
-
-      // columns
-      for (var y = 0; y < rpgtoolkit.tileSize; y++) {
-
-        // store RGB value
-        tileRow.push([
-          this.file.readUint8(),
-          this.file.readUint8(),
-          this.file.readUint8()
-        ]);
-      }
-
-      tile.push(tileRow);
-    }
-
-    this.tiles.push(tile);
-  }
+    var tileSet = JSON.parse(req.responseText);
+    this.images = tileSet.images;
+    this.tileWidth = tileSet.tileWidth;
+    this.tileHeight = tileSet.tileHeight;
 }
+
+Tileset.prototype.load = function () {
+    this.img = Crafty.assets[this.images[0]];
+
+    this.tileRows = Math.floor(this.img.height / this.tileHeight);
+    this.tileColumns = Math.floor(this.img.width / this.tileWidth);
+    this.count = this.tileRows * this.tileColumns;
+
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.ctx.drawImage(this.img, 0, 0);
+};
+
+Tileset.prototype.getTile = function (index) {
+    // Converted 1D index to 2D cooridnates.
+    var x = index % this.tileColumns;
+    var y = Math.floor(index / this.tileColumns);
+
+    var tile = this.ctx.getImageData(
+            x * this.tileWidth, y * this.tileHeight, 
+            this.tileWidth, this.tileHeight);
+
+    return tile;
+};
